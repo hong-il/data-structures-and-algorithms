493e22797911d29923e5fde55ec786ca11dd5842
KIM HONGIL <clroyan01@gmail.com>
2021-03-27 18:11:44 +0900

    Printer(queue) - Solution

--- src/Queue/level2/Printer.java
+++ src/Queue/level2/Printer.java
@@ -7,6 +7,25 @@ class Printer {
     private Queue<Document> queue;
     private int answer;
 
+    public int solution(int[] priorities, int location) {
+        queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++)
+            queue.offer(new Document(priorities[i], i));
+        while (!queue.isEmpty()) {
+            Document head = queue.poll();
+            if (isPrintable(head)) {
+                answer++;
+                if (head.index == location)
+                    break;
+            } else queue.offer(head);
+        }
+        return answer;
+    }
+
+    public boolean isPrintable(Document head) {
+        return queue.stream().noneMatch(document -> document.priority > head.priority);
+    }
+
     class Document {
         int priority;
         int index;
