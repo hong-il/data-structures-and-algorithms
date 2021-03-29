bdc398a467d0a21dba2b3a02a57b32c562e01d4c
KIM HONGIL <clroyan01@gmail.com>
2021-03-29 22:54:32 +0900

    [Map] Open Chat - Solution

--- src/map/OpenChat.java
+++ src/map/OpenChat.java
@@ -1,8 +1,53 @@
 package map;
 
+import java.util.*;
+
+
+/*
+* 제한사항
+- record는 다음과 같은 문자열이 담긴 배열이며, 길이는 1 이상 100,000 이하이다.
+- 다음은 record에 담긴 문자열에 대한 설명이다.
+- 모든 유저는 [유저 아이디]로 구분한다.
+- [유저 아이디] 사용자가 [닉네임]으로 채팅방에 입장 - "Enter [유저 아이디] [닉네임]" (ex. "Enter uid1234 Muzi")
+- [유저 아이디] 사용자가 채팅방에서 퇴장 - "Leave [유저 아이디]" (ex. "Leave uid1234")
+- [유저 아이디] 사용자가 닉네임을 [닉네임]으로 변경 - "Change [유저 아이디] [닉네임]" (ex. "Change uid1234 Muzi")
+- 첫 단어는 Enter, Leave, Change 중 하나이다.
+- 각 단어는 공백으로 구분되어 있으며, 알파벳 대문자, 소문자, 숫자로만 이루어져있다.
+- 유저 아이디와 닉네임은 알파벳 대문자, 소문자를 구별한다.
+- 유저 아이디와 닉네임의 길이는 1 이상 10 이하이다.
+- 채팅방에서 나간 유저가 닉네임을 변경하는 등 잘못 된 입력은 주어지지 않는다.
+* */
 public class OpenChat {
 
     public String[] solution(String[] record) {
-        return new String[1];
+        List<String> ansList = new ArrayList<>();
+        Map<String, String> idMap = new HashMap<>();
+
+        for (String str : record) {
+            StringTokenizer tokenizer = new StringTokenizer(str);
+            String cmd = tokenizer.nextToken();
+            if (cmd.equals("Enter") || cmd.equals("Change")) {
+                String id = tokenizer.nextToken();
+                String name = tokenizer.nextToken();
+                idMap.put(id, name);
+            }
+        }
+
+        for (String str : record) {
+            StringTokenizer tokenizer = new StringTokenizer(str);
+            String cmd = tokenizer.nextToken();
+            if (cmd.equals("Enter")) {
+                String id = tokenizer.nextToken();
+                ansList.add(idMap.get(id) + "님이 들어왔습니다.");
+            } else if (cmd.equals("Leave")) {
+                String id = tokenizer.nextToken();
+                ansList.add(idMap.get(id) + "님이 나가셨습니다.");
+            }
+        }
+
+        String[] answer = new String[ansList.size()];
+        ansList.toArray(answer);
+
+        return answer;
     }
 }
