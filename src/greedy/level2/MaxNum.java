6154e747cf6b070b2f4969073a169e1a5702dbe2
KIM HONGIL <clroyan01@gmail.com>
2021-03-30 22:19:37 +0900

    [Greedy] Find Max Number Index - Initial Commit

---
+++ src/greedy/level2/MaxNum.java
@@ -0,0 +1,12 @@
+package greedy.level2;
+
+public class MaxNum {
+
+    public String solution(String number, int k) {
+        return null;
+    }
+
+    public int findMaxNumIndex(String number, int startIndex, int endIndex) {
+        return 0;
+    }
+}
