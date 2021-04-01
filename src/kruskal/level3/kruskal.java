24e80f517794720e46dab55d59ed8e1036128732
KIM HONGIL <clroyan01@gmail.com>
2021-04-01 23:44:40 +0900

    [Greedy] Kruskal - Solution

--- src/kruskal/level3/kruskal.java
+++ src/kruskal/level3/kruskal.java
@@ -1,5 +1,8 @@
 package kruskal.level3;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /*
 * n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때, 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요.
 다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다. 예를 들어 A 섬과 B 섬 사이에 다리가 있고, B 섬과 C 섬 사이에 다리가 있으면 A 섬과 C 섬은 서로 통행 가능합니다.
@@ -14,21 +17,39 @@ package kruskal.level3;
 * */
 public class kruskal {
 
+    private static final int COST = 2;
+
     public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (Comparator.comparingInt(o -> o[COST])));
+        int[] parentsNodes = createCycleTable(n);
         int answer = 0;
+        for(int[] edge : costs) {
+            if (parentsNodes[edge[0]] != parentsNodes[edge[1]]) {
+                updateConnectedNodes(parentsNodes, edge[1], edge[0]);
+                answer += edge[COST];
+            }
+            if (pathCreated(parentsNodes)) break;
+        }
         return answer;
     }
 
     private void updateConnectedNodes(int[] parentNodes, int from, int to) {
-
+        int target = parentNodes[from];
+        for (int i = 0; i < parentNodes.length; i++) {
+            if (parentNodes[i] == target)
+                parentNodes[i] = parentNodes[to];
+        }
     }
 
     private boolean pathCreated(int[] parentNodes) {
+        for (int i = 0; i < parentNodes.length; i++)
+            if (parentNodes[i] != parentNodes[i + 1]) return false;
         return true;
     }
 
     private int[] createCycleTable(int n) {
         int[] result = new int[n];
+        for (int i = 0; i < n; i++) result[i] = i;
         return result;
     }
 }
